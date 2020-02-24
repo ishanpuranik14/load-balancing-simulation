e3253ae4188953e67bcbd26731f65221b63a529a
rocknrolla14 <ishanpuranik14@gmail.com>
2020-02-23 17:55:38 -0800

    Made poisson generator work using in-built libraries

--- poisson_test.cpp
+++ poisson_test.cpp
@@ -3,10 +3,18 @@
 //
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
+random_device rd;
+mt19937 gen(rd());
+
 int main(){
-    cout<<"Hello World"<<endl;
+    poisson_distribution<int> pd(5);
+    long double sum = 0;
+    int num_iterations = 100000;
+    for(int i=0; i< num_iterations; i++){
+        sum += pd(gen);
+    }
+    cout<<"Average: "<<sum/num_iterations<<endl;
     return 0;
 }
