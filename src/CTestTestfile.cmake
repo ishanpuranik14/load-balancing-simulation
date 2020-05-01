fbd1a0fb1f13b5c082870648e964452da0d354a1
Gitanshu Sardana <gitanshusardana@gmail.com>
2020-05-01 16:24:08 -0700

    Revert "Added features for config read from csv file"
    This reverts commit 668a03b1d56a862af290121cf8b2b1140a2c9474.

--- src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vikas/load-balancing-simulation
-# Build directory: /home/vikas/load-balancing-simulation/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
