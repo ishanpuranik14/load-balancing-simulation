66bbb5cfaae84e6c80c577ac8e643968aa8ef53b
vikas <vikasnyamati96@gmail.com>
2020-05-08 23:56:19 -0700

    changes in graphPlot

--- src/graphPlot.py
+++ src/graphPlot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import csv
 import os
 
-def plot_graph(plots,path):
+def plot_graph(plots,path,iteration):
     x = []
     y = []
     i = 0 #This variable is used to skip csv headers from being read
@@ -11,15 +11,34 @@ def plot_graph(plots,path):
             x.append(float(row[0])) #change this according to whatever needs to be plot from csv
             y.append(float(row[6])) #change this according to whatever needs to be plot from csv
         i = 1 #Value changed to 1 after the first loop
-    plt.plot(x,y)
+    plt.plot(x,y,label=iteration)
     plt.xlabel('x')
     plt.ylabel('y')
     plt.title('Title')
-    #plt.legend()
-    plt.gcf()
-    plt.draw()
+    plt.legend()
+    #plt.gcf()
+    #plt.draw()
     file = "Graph.png"
     plt.savefig(path+file)
+    # plt.clf()
+
+def plot_combined_graph(plots,path,filename,iteration):
+    x = []
+    y = []
+    i = 0 #This variable is used to skip csv headers from being read
+    for row in plots:
+        if(i!=0):
+            x.append(float(row[0])) #change this according to whatever needs to be plot from csv
+            y.append(float(row[6])) #change this according to whatever needs to be plot from csv
+        i = 1 #Value changed to 1 after the first loop
+    plt.plot(x,y,label=iteration)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Title')
+    plt.legend()
+    #plt.gcf()
+    #plt.draw()
+    plt.savefig(path+filename)
     # plt.clf()
 
 rootDir = "config_0_0_x_results/"
@@ -31,7 +50,8 @@ for subdirs,dirs,files in os.walk(rootDir):
         #to plot graphs for servers
         with open(serverStats,'r') as csvfile:
             plots = csv.reader(csvfile, delimiter=',')
-            plot_graph(plots,serverStats)
+            #plot_graph(plots,serverStats,d)
+            plot_combined_graph(plots,rootDir,"combinedServerStats",d)
 
         # #to plot graphs for overall system
         # with open(overallStats,'r') as csvfile:
